bd5e89f46a027de430b5e6f3e8c3d71e173ea41d
Hattou Faïssal <faissal.hattou@gmail.com>
2024-10-30 16:39:43 +0100

    chore: add variables to global css file and use them in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,54 +11,31 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
+        primary: "hsl(var(--primary))",
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          hover: "var(--secondary-hover)",
         },
         accent: {
           DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          hover: "hsl(var(--accent-hover))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          "1": "hsl(var(--chart-1))",
-          "2": "hsl(var(--chart-2))",
-          "3": "hsl(var(--chart-3))",
-          "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))",
-        },
+        invert: "hsl(var(--invert))",
+        danger: "hsl(var(--danger))",
       },
       borderRadius: {
         lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        "border-accent":
+          "0px 0px 0px 1px rgba(228, 92, 1, 0.76), 0px 1px 2px 0px rgba(62, 61, 58, 0.4)",
+        "border-secondary":
+          "0px 0px 0px 1px rgba(81, 80, 75, 0.08), 0px 1px 2px 0px rgba(81, 77, 42, 0.12)",
+        focus: "0px 0px 0px 4px var(--shadow-focus)",
       },
     },
   },
   plugins: [tailwindcssAnimatePlugin],
 };
+
 export default config;
